093d799a34b39e292225b206e938d313c8326bb1
unknown <fonke1982@gmail.com>
2023-02-01 11:03:00 +0100

    configuration properties

---
+++ src/test/java/com/stockback/StockBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
